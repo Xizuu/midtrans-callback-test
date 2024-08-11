2b888bd31bfe43b8521ca2e712598ed8568f5895
Raihan Pratama Putra <98379156+Xizuu@users.noreply.github.com>
2024-08-11 15:22:14 +0800

    auto update (WIP)

--- src/thanks.jsx
+++ src/thanks.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Thanks() {
     const url = new URLSearchParams(window.location.search);
     const orderId = url.get('order_id');
-    const trans_status = url.get('transaction_status');
+    const initialTransStatus = url.get('transaction_status');
+    
+    const [transStatus, setTransStatus] = useState(initialTransStatus);
 
     const getStatusMessage = (transaction_status) => {
         if (transaction_status === 'pending') {
@@ -17,7 +19,34 @@ function Thanks() {
         }
     };
 
-    const statusMessage = getStatusMessage(trans_status);
+    const statusMessage = getStatusMessage(transStatus);
+
+    useEffect(() => {
+        const checkTransactionStatus = async () => {
+            try {
+                const response = await fetch('https://midtrans-callback-test.vercel.app/transaction', {
+                        method: 'POST',
+                        body: JSON.stringify({ orderId })
+                    }
+                );
+                const data = await response.json();
+                
+                if (data.transaction_status === 'settlement') {
+                    setTransStatus('settlement');
+                }
+            } catch (error) {
+                console.error('Error fetching transaction status:', error);
+            }
+        };
+
+        const intervalId = setInterval(checkTransactionStatus, 5000);
+
+        if (transStatus === 'settlement') {
+            clearInterval(intervalId);
+        }
+
+        return () => clearInterval(intervalId);
+    }, [orderId, transStatus]);
 
     return (
         <div className="bg-gray-100 h-screen">
@@ -27,7 +56,7 @@ function Thanks() {
                         {statusMessage}
                     </h3>
                     {statusMessage !== 'Unknown Transaction Status' && (
-                        <p className="text-gray-600 my-2">Transaction ID: {orderId}</p>
+                        <p className="text-gray-600 my-2">Transaction ID: {orderId ?? 'Null'}</p>
                     )}
                     <div className="py-10 text-center">
                         <a href="#" className="px-12 bg-indigo-600 hover:bg-indigo-500 text-white font-semibold py-3">
